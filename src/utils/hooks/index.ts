33a45bff0ff77d18041da715dd89f8bb23cc2ad8
Andrés Siri <andres.siri@hotmail.com>
2023-06-09 09:28:51 -0300

    Create handle response hook

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useHandleResponseEffect } from "./useHandleResponseEffect";
 export { default as useMockRequest } from "./useMockRequest";
 export { default as useServiceInstance } from "./useServiceInstance";
 export * from "./authHooks";
