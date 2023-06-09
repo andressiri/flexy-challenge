4dda23706a597b2475363aa3453fc2e503dfa23c
Andrés Siri <andres.siri@hotmail.com>
2023-06-09 09:11:04 -0300

    Improve title component

--- src/components/Atoms/Title/index.tsx
+++ src/components/Atoms/Title/index.tsx
@@ -4,6 +4,7 @@ import { StyledTitle } from "./styledComponents";
 interface Props {
   text: string;
 }
+
 const Title: FC<Props> = ({ text }) => {
   return <StyledTitle variant="h1">{text}</StyledTitle>;
 };
--- src/components/Atoms/Title/styledComponents.ts
+++ src/components/Atoms/Title/styledComponents.ts
@@ -2,7 +2,12 @@ import { styled } from "@mui/material/styles";
 import { Typography } from "@mui/material";
 
 export const StyledTitle = styled(Typography)(({ theme }) => ({
+  maxWidth: "calc(100% - 60px)",
   color: theme.palette.primary.dark,
+  textAlign: "center",
   fontSize: "60px",
   fontWeight: 800,
+  "@media (max-width: 1200px)": {
+    fontSize: "calc(60px - (1200px - 100vw) * 0.03)",
+  },
 }));
