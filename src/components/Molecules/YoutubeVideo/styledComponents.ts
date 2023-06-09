72bc29560175f790d36374f7802a6d51da27051d
Andrés Siri <andres.siri@hotmail.com>
2023-06-09 09:16:35 -0300

    Improve youtube video component

--- src/components/Molecules/YoutubeVideo/styledComponents.ts
+++ src/components/Molecules/YoutubeVideo/styledComponents.ts
@@ -1,7 +1,7 @@
 import { styled } from "@mui/material/styles";
 
 export const StyledIframe = styled("iframe")(({ theme }) => ({
-  maxWidth: "100%",
+  maxWidth: "calc(100% - 60px)",
   margin: "0px 30px",
   borderRadius: theme.custom.radius.medium,
 }));
