67169a0824433bd11857d3066b4ca3a7d6f96feb
Andrés Siri <andres.siri@hotmail.com>
2023-06-09 09:19:47 -0300

    Create basic form constructor component

--- src/components/Organisms/index.ts
+++ src/components/Organisms/index.ts
@@ -1,2 +1,3 @@
+export { default as BasicFormConstructor } from "./BasicFormConstructor";
 export { default as LogButtons } from "./LogButtons";
 export { default as MobileDrawer } from "./MobileDrawer";
