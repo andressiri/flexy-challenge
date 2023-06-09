c710b689842a1e50207dbe8b79e117c32fc5f50f
Andrés Siri <andres.siri@hotmail.com>
2023-06-09 09:32:25 -0300

    Create login page

--- src/components/Templates/LoginPage/index.tsx
+++ src/components/Templates/LoginPage/index.tsx
@@ -1,10 +1,26 @@
-import { Container, Title } from "./styledComponents";
+import { FC } from "react";
+import { useGetLoginComponentsProps } from "../../../utils/hooks";
+import { RememberMe } from "../../Molecules";
+import { FormWithPicturePage } from "../../Organisms";
+import { REGISTER_ROUTE } from "../../../config/constants";
 
-const LoginPage = () => {
+const LoginPage: FC = () => {
+  const { title, subtitle, formProps } = useGetLoginComponentsProps();
+  const bottomText = "¿No tenés una cuenta?";
+  const bottomLink = `/${REGISTER_ROUTE}`;
+  const bottomLinkText = "Registrate";
+
   return (
-    <Container component="section">
-      <Title variant="h1">Login Page</Title>
-    </Container>
+    <FormWithPicturePage
+      title={title}
+      subtitle={subtitle}
+      formProps={formProps}
+      bottomText={bottomText}
+      bottomLink={bottomLink}
+      bottomLinkText={bottomLinkText}
+    >
+      <RememberMe />
+    </FormWithPicturePage>
   );
 };
 
