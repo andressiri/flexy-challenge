19e2ca36e3de905cd5089dcba5cfbd60f7303001
Andrés Siri <andres.siri@hotmail.com>
2023-06-09 09:32:04 -0300

    Create register page

--- src/components/Templates/RegisterPage/index.tsx
+++ src/components/Templates/RegisterPage/index.tsx
@@ -1,10 +1,35 @@
-import { Container, Title } from "./styledComponents";
+import { FC, useContext, useMemo } from "react";
+import { GeneralContext } from "../../../contexts";
+import { useGetRegisterComponentsProps } from "../../../utils/hooks";
+import { FormWithPicturePage } from "../../Organisms";
+import { ChildrenText } from "./styledComponents";
+import { LOGIN_ROUTE } from "../../../config/constants";
 
-const RegisterPage = () => {
+const RegisterPage: FC = () => {
+  const { viewportWidth } = useContext(GeneralContext);
+  const { title, subtitle, imageProps, formProps } =
+    useGetRegisterComponentsProps();
+  const bottomText = "¿Ya tenés una cuenta?";
+  const bottomLink = `/${LOGIN_ROUTE}`;
+  const bottomLinkText = "Iniciá sesión";
+  const childrenText = useMemo(() => {
+    return viewportWidth > 768
+      ? "Debe tener al menos 8 caracteres."
+      : "¿Olvidaste tu contraseña?";
+  }, [viewportWidth]);
+
   return (
-    <Container component="section">
-      <Title variant="h1">Register Page</Title>
-    </Container>
+    <FormWithPicturePage
+      title={title}
+      subtitle={subtitle}
+      imageProps={imageProps}
+      formProps={formProps}
+      bottomText={bottomText}
+      bottomLink={bottomLink}
+      bottomLinkText={bottomLinkText}
+    >
+      <ChildrenText>{childrenText}</ChildrenText>
+    </FormWithPicturePage>
   );
 };
 
--- src/components/Templates/RegisterPage/styledComponents.ts
+++ src/components/Templates/RegisterPage/styledComponents.ts
@@ -1,12 +1,16 @@
 import { styled } from "@mui/material/styles";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 
-export const Container = styled(Box)(({ theme }) => ({
-  paddingTop: theme.custom.header.height,
-}));
-
-export const Title = styled(Typography)(({ theme }) => ({
-  color: theme.palette.primary.main,
-  fontSize: "60px",
-  fontWeight: 800,
+export const ChildrenText = styled(Typography)(({ theme }) => ({
+  margin: "-13px auto 25px auto",
+  color: theme.custom.colors.greyscale500,
+  textAlign: "center",
+  fontSize: "14px",
+  fontWeright: 400,
+  lineHeight: "19.6px",
+  "@media (max-width: 768px)": {
+    color: theme.palette.primary.main,
+    margin: "-10px auto 27px auto",
+    fontWeight: 500,
+  },
 }));
